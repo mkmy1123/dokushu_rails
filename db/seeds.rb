e061a5df00b8daa6702c69822a45118716d2348a
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-12 23:31:09 +0900

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+areas = ["東京", "札幌", "仙台", "横浜", "名古屋", "大阪", "福岡", "広島"]
+30.times do |n|
+  ar = areas.sample
+  Room.create!(
+    name: "#{ar}会議室 No#{n}",
+    place: "#{ar}#{n}",
+    number: 10,
+    terms_of_use: "#{ar}の利用要綱を承諾すること"
+  )
+end
+puts "30 件データ作成完了"
