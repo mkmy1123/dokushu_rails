52eced5f7ca61eea1c8a9cfc456c98fbaa52b46a
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-13 21:58:08 +0900

    finish learning around active record

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,22 @@
 class Room < ApplicationRecord
+  validates :name, :place, :number, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :name, format: { with: /\+?#[0-9][0-9]/, message: "会議室名が正しくありません" }
+  validates :place, inclusion: ["東京", "大阪", "福岡", "札幌", "仙台", "名古屋", "金沢"]
+  validates :number, numericality: { greater_than: 4, less_than: 31 }
+  validate :number_must_be_5_multiple
+
+  before_validation :remove_space
+
+  private
+    def remove_space
+      ary = self.name.split(' ')
+      self.name = ary.join('_')
+    end
+
+    def number_must_be_5_multiple
+      if number.present? && number % 5 != 0
+        errors.add(:number, "収容人数は5の倍数で指定してください")
+      end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,16 @@ areas = ["東京", "札幌", "仙台", "横浜", "名古屋", "大阪", "福岡"
   )
 end
 puts "30 件データ作成完了"
+
+20.times do |n|
+  rdate = Time.now + n.days
+  Entry.create!(
+    reserved_date: rdate,
+    user_name: "ABC#{n}",
+    user_email: "abc#{n}@sample.com",
+    usage_time: 2,
+    room_id: 1,
+    people: 5
+  )
+end
+puts "20 件データ作成完了"
