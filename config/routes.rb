dd6d6d108cce4a65b8626ce3a449df0e8eba8f35
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-15 17:26:54 +0900

    finish learning routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  root to: 'top#index'
   resources :rooms
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :entries, only: [:new, :create, :destroy, :index], path: :rentals do
+    post 'comfirm', on: :collection
+  end
 end
